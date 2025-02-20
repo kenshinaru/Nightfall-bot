f86f4377c2a689ad14cf84c5765d24842e45bd06
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-02-20 09:29:25 +0700

    Update serialize.js

--- utils/serialize.js
+++ utils/serialize.js
@@ -2,6 +2,31 @@ import baileys, { extractMessageContent } from "baileys";
 import set from '../setting.js'
 import func from './functions.js'
 
+export function getContentType(a) {
+    if (a) {
+        const keys = Object.keys(a);
+        const key = keys.find(k => (k === 'conversation' || k.endsWith('Message') || k.includes('V2') || k.includes('V3')) && k !== 'senderKeyDistributionMessage');
+        return key ? key : keys[0];
+    }
+}
+
+export function parseMessage(content) {
+    content = extractMessageContent(content);
+
+    if (content && content.viewOnceMessageV2Extension) {
+        content = content.viewOnceMessageV2Extension.message;
+    }
+    if (content && content.protocolMessage && content.protocolMessage.type == 14) {
+        let type = getContentType(content.protocolMessage);
+        content = content.protocolMessage[type];
+    }
+    if (content && content.message) {
+        let type = getContentType(content.message);
+        content = content.message[type];
+    }
+
+    return content;
+}
 export async function msg(sock, m, store) {
 	if (m.key) {
 		m.id = m.key.id;
@@ -12,20 +37,20 @@ export async function msg(sock, m, store) {
 		m.sender = m.key.fromMe ? sock.decodeJid(sock.user.id) : (m.key.participant);
 		m.isOwner = [sock.decodeJid(sock.user.id).replace(/@.+/, ''), set.owner].map(v => v + '@s.whatsapp.net').includes(m.sender)
 
-		if (m.isGroup) {
-			  if (!(m.chat in store.groupMetadata)) store.groupMetadata[m.chat] = await sock.groupMetadata(m.chat);
-				const groupMetadata = store.groupMetadata[m.chat];
-				m.metadata = groupMetadata;
-				m.participants = groupMetadata ? groupMetadata.participants : [];
-				m.admins = (m.participants.reduce((memberAdmin, memberNow) => (memberNow.admin ? memberAdmin.push({ id: memberNow.id, admin: memberNow.admin }) : [...memberAdmin]) && memberAdmin, []));
-				m.isAdmin = !!m.admins.find((member) => member.id === m.sender);
-				m.isBotAdmin = !!m.admins.find((member) => member.id === sock.decodeJid(sock.user.id));
-			}
+	if (m.isGroup) {
+        if (!(m.chat in store.groupMetadata)) store.groupMetadata[m.chat] = await sock.groupMetadata(m.chat);
+	    const groupMetadata = store.groupMetadata[m.chat];
+		m.metadata = groupMetadata;
+		m.participants = groupMetadata ? groupMetadata.participants : [];
+		m.admins = (m.participants.reduce((memberAdmin, memberNow) => (memberNow.admin ? memberAdmin.push({ id: memberNow.id, admin: memberNow.admin }) : [...memberAdmin]) && memberAdmin, []));
+		m.isAdmin = !!m.admins.find((member) => member.id === m.sender);
+		m.isBotAdmin = !!m.admins.find((member) => member.id === sock.decodeJid(sock.user.id));
+	     }
 	}
 
 	if (m.message) {
-		m.type = sock.getContentType(m.message) || Object.keys(m.message)[0];
-		m.msg = extractMessageContent(m.message[m.type]);
+		m.type = getContentType(m.message) || Object.keys(m.message)[0];
+                m.msg = parseMessage(m.message[m.type]) || m.message[m.type]
 		m.body = m.type === "conversation" ? m.message?.conversation
 		: m.type == "imageMessage" ? m.message?.imageMessage?.caption
 		: m.type == "videoMessage" ? m.message?.videoMessage?.caption
@@ -36,7 +61,8 @@ export async function msg(sock, m, store) {
 		: m.type == "messageContextInfo" ? m.message?.listResponseMessage?.singleSelectReply?.selectedRowId || m.message?.buttonsResponseMessage?.selectedButtonId
 		: m.type == "interactiveResponseMessage" ? (JSON.parse(m.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson)).id : ""
 		m.mentions = m.msg?.contextInfo?.mentionedJid || []
-                m.isMedia = !!m.msg?.mimetype || !!m.msg?.thumbnailDirectPath;
+                m.expiration = m.msg?.contextInfo?.expiration || 0;
+		m.isMedia = !!m.msg?.mimetype || !!m.msg?.thumbnailDirectPath;
                   if (m.isMedia) {
                 m.viewOnce = m.msg?.viewOnce
                 m.mime = m.msg?.mimetype;
@@ -49,7 +75,7 @@ export async function msg(sock, m, store) {
                 }
 		m.delete = async() => await sock.sendMessage(m.chat, { delete: m.key });
 		m.download = async() => await sock.downloadMediaMessage(m);
-              m.react = async (emot) => await sock.sendMessage(m.chat, { react: { text: emot, key: m.key }})
+                m.react = async (emot) => await sock.sendMessage(m.chat, { react: { text: emot, key: m.key }})
               
 		m.isQuoted = !!m.msg?.contextInfo?.quotedMessage;
 
@@ -68,25 +94,21 @@ export async function msg(sock, m, store) {
 		}
 	}
 
-	m.reply = async(txt = "", options = {}) => {
-		return await sock.sendMessage(options.from || m.chat, {
-			text: txt,
-			contextInfo: {
-				mentionedJid: options.mentions || sock.parseMentions(txt) || [m.sender],
-				remoteJid: options.remote || null
-			}
-		}, {
-			quoted: options.quoted || m
-		})
-	}
+	m.reply = async (text, options = {}) => {
+            if (typeof text === "string") {
+                return await sock.sendMessage(m.from, { text, ...options }, { quoted: m, ephemeralExpiration: m.expiration, ...options });
+            } else if (typeof text === "object" && typeof text !== "string") {
+                return sock.sendMessage(m.from, { ...text, ...options }, { quoted: m, ephemeralExpiration: m.expiration, ...options });
+            }
+        }
 	
 	m.report = async (text, options = {}) => {
-      if (typeof text == "object") {
-				return sock.reply(set.owner+"@s.whatsapp.net", func.format(text), m, { mentions: [set.owner+"@s.whatsapp.net", ...sock.parseMentions(func.format(text))], ...options});
-			} else {
-				return sock.reply(set.owner+"@s.whatsapp.net", text,  m, {...options} );
-			}
-		}
+            if (typeof text == "object") {
+		return sock.reply(set.owner+"@s.whatsapp.net", func.format(text), m, { mentions: [set.owner+"@s.whatsapp.net", ...sock.parseMentions(func.format(text))], ...options});
+	     } else {
+		return sock.reply(set.owner+"@s.whatsapp.net", text,  m, {...options} );
+	      }
+	}
 
 	return m;
 }
