41c4df99fdc5283fdb6c0e76fa20da810eb7803c
kenshinaru <121244764+kenshinaru@users.noreply.github.com>
2025-03-05 07:11:04 +0700

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -3,31 +3,43 @@ import moment from 'moment-timezone';
 
 moment.tz.setDefault('Asia/Jakarta').locale('id');
 
-export default async (sock, m, isCmd) => {
-    const formattedTime = moment().format('HH:mm:ss');
-    const cmdName = isCmd ? m.body.slice(1).split(/\s+/)[0] : '';
-    const isGc = m.chat.endsWith('@g.us');
-    const gcName = isGc ? (await sock.groupMetadata(m.chat)).subject : '';
-    const pushName = m.pushName || '-';
+export default async (x, y, z) => {
+    const t = moment().format('HH:mm:ss');
+    const a = z ? y.body.slice(1).split(/\s+/)[0] : '';
+    const b = y.chat?.endsWith('@g.us');
+    let c = '-';
 
-    if (isCmd) {
+    if (b) {
+        try {
+            const meta = await x.groupMetadata[y.chat]
+            c = meta?.subject || '-';
+        } catch (err) {
+            c = '-';
+        }
+    }
+
+    const d = y.pushName || '-';
+
+    if (z) {
         console.log(
             chalk.white.bold('--- CMD ENTRY ---') +
-            `\nTime    : ${chalk.green(formattedTime)}` +
-            `\nUser    : ${chalk.cyan(pushName)}` +
-            `\nID      : ${chalk.gray(m.chat)}` +
-            `\nType    : ${chalk.yellow(m.type)}` +
-            `\nBody    : ${chalk.magenta(cmdName)}` +
+            `\nTime    : ${chalk.green(t)}` +
+            `\nUser    : ${chalk.cyan(d)}` +
+            `\nID      : ${chalk.gray(y.chat)}` +
+            `\nGroup   : ${chalk.blue(c)}` +
+            `\nType    : ${chalk.yellow(y.type)}` +
+            `\nBody    : ${chalk.magenta(a)}` +
             `\n---------------`
         );
     } else {
         console.log(
             chalk.white.bold('--- MSG ENTRY ---') +
-            `\nTime    : ${chalk.green(formattedTime)}` +
-            `\nUser    : ${chalk.cyan(pushName)}` +
-            `\nID      : ${chalk.gray(m.chat)}` +
-            `\nType    : ${chalk.yellow(m.type)}` +
-            `\nBody    : ${chalk.magenta((typeof m.text === 'string' && m.text) ? m.text : '-')}` +
+            `\nTime    : ${chalk.green(t)}` +
+            `\nUser    : ${chalk.cyan(d)}` +
+            `\nID      : ${chalk.gray(y.chat)}` +
+            `\nGroup   : ${chalk.blue(c)}` +
+            `\nType    : ${chalk.yellow(y.type)}` +
+            `\nBody    : ${chalk.magenta((typeof y.text === 'string' && y.text) ? y.text : '-')}` +
             `\n---------------`
         );
     }
